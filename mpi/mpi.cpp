d01d4833bf3eae72a038d6fa049e7e9e834e3911
gconn <alina.pashkova.03@mail.ru>
2023-03-21 13:43:32 +0700

    FInished work

--- mpi/mpi.cpp
+++ mpi/mpi.cpp
@@ -5,7 +5,8 @@
 
 //  mpicxx mpi.cpp -o mpi && mpiexec -n 4 ./mpi
 
-void setMatrixParts(int* lineSize, int* lineOffset, int N, int processSize);
+void setMatrixParts(int* lineSize, int* lineOffset, int* sendCounts,
+                    int* displs, int N, int processSize);
 double* generateAMatix();
 double* generateMatix(int v);
 void getAxb(const double* Achunk, const double* x, const double* b,
@@ -18,104 +19,59 @@ double* simpleIterationMethod(double* Achunk, double* b, double* x,
 double norm(double* vec, int size);
 
 constexpr double eps = 0.00001f, tau = 0.0001f;
-constexpr int N = 10;
+constexpr int N = 1024 * 13;
 int main(int argc, char* argv[]) {
   int processRank;  // current
   int processSize;  // all
   double startTime, endTime;
-  int *lineOffset, *lineSize;
+  int *lineOffset, *lineSize, *sendCounts, *displs;
   double* partBuffer;
   double *b, *X, *A;
   double bNorm;
 
   A = generateAMatix();
-
+  b = generateMatix(N + 1);
+  bNorm = norm(b, N);
+  X = generateMatix(0);
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &processRank);
   MPI_Comm_size(MPI_COMM_WORLD, &processSize);
   lineSize = new int[processSize];
   lineOffset = new int[processSize];
-  setMatrixParts(lineSize, lineOffset, N, processSize);
-  // std::cout << "RANK " << processRank << " " << lineSize[processRank] << " "
-  //           << N << "\n";
-  // std::cout << "RANK " << lineSize[processRank] * N << "\n";
-  double* AChunk = new double[(lineSize[processRank])];
-  for (int i = 0; i < (lineSize[processRank] * N); ++i) {
+  sendCounts = new int[processSize];
+  displs = new int[processSize];
+  setMatrixParts(lineSize, lineOffset, sendCounts, displs, N, processSize);
+  double* AChunk = new double[(sendCounts[processRank])];
+  for (int i = 0; i < (sendCounts[processRank]); ++i) {
     AChunk[i] = -1;
   }
   startTime = MPI_Wtime();
+  MPI_Scatterv(A, sendCounts, displs, MPI_DOUBLE, AChunk,
+               sendCounts[processRank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-  // generateAMatixChunk(lineSize[processRank], lineOffset[processRank]);
-  if (processRank == 0) {
-    for (int i = 0; i < N; ++i) {
-      // for (int j = 0; j < N; ++j) {
-      //   std::cout << A[i * N + j] << " ";
-      // }
-      // std::cout << "\n";
-      printf("sendcounts[%d] = %d\tdispls[%d] = %d\n", i, lineSize[i], i,
-             lineOffset[i]);
-    }
-
-    // b = generateMatix(N + 1);
-    // bNorm = norm(b, N);
-    // X = generateMatix(0);
-    // MPI_Bcast(b, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    // MPI_Bcast(X, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    // MPI_Bcast(&bNorm, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  }
-  // MPI_Scatterv(A, lineSize, lineOffset, MPI_DOUBLE, AChunk,
-  //              lineSize[processRank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-  // std::cout << "Raaank " << processRank << "\n";
-  // for (int i = 0; i < lineSize[processRank] / N; ++i) {
-  //   for (int j = 0; j < N; ++j)
-  //     std::cout << i << " " << j << " --> " << AChunk[i * N + j] <<
-  //     std::endl;
-  // }
-  // b = generateMatix(N + 1);
-  // bNorm = norm(b, N);
-  // X = generateMatix(0);
-
-  // if (processRank == 0)
-  //   for (int i = 0; i < lineOffset[processRank] * N; ++i)
-  //     std::cout << AChunk[i] << " ";
-  // std::cout << "\n";
-
-  // simpleIterationMethod(AChunk, b, X, bNorm, lineSize, lineOffset);
+  simpleIterationMethod(AChunk, b, X, bNorm, lineSize, lineOffset);
 
   endTime = MPI_Wtime();
-  if (processRank == 0)
-    // std::cout << "Total time is " << endTime - startTime << std::endl;
-    std::cout << endTime - startTime << "\n";
+  if (processRank == 0) std::cout << endTime - startTime << "\n";
 
   MPI_Finalize();
 
   return 0;
 }
 
-void setMatrixParts(int* lineSize, int* lineOffset, int N, int processSize) {
+void setMatrixParts(int* lineSize, int* lineOffset, int* sendCounts,
+                    int* displs, int N, int processSize) {
   int offset = 0, bufSize = N / processSize;
   int bufRemainder = N % processSize;
   for (int i = 0; i < processSize; ++i) {
     lineOffset[i] = offset;
     lineSize[i] = bufSize;
     if (i < bufRemainder) ++lineSize[i];
-    lineSize[i] *= N;
-    std::cout << lineSize[i] << " " << lineOffset[i] << "\n";
-
+    sendCounts[i] = lineSize[i] * N;
+    displs[i] = offset * N;
     offset += lineSize[i];
   }
 }
-
-// double* generateAMatixChunk(int lineSize, int offset) {
-//   double* AChunk = new double[lineSize * N];
-//   for (int i = 0; i < lineSize; ++i) {
-//     for (int j = 0; j < N; ++j) AChunk[i * N + j] = 1;
-
-//     AChunk[i * N + offset + i] = 2;  // Чтобы поставить на нужное место
-//   }
-//   return AChunk;
-// }
 
 double* generateMatix(int val) {
   double* x = new double[N];
@@ -129,8 +85,7 @@ double* generateAMatix() {
   double* A = new double[N * N];
   for (int i = 0; i < N; ++i) {
     for (int j = 0; j < N; ++j) A[i * N + j] = 1;
-
-    A[i * N + i] = 2;  // Чтобы поставить на нужное место
+    A[i * N + i] = 2;
   }
   return A;
 }
@@ -142,11 +97,12 @@ double* simpleIterationMethod(double* Achunk, double* b, double* x,
   double acc = eps * eps;
   int processRank;  // current
   MPI_Comm_rank(MPI_COMM_WORLD, &processRank);
-  int numberOfLines = line_count[processRank], offset = line_count[processRank];
+  int numberOfLines = line_count[processRank],
+      offset = line_offsets[processRank];
   int iCount = 0;
-  double* AxbChunk = new double[numberOfLines];
+  double* AxbChunk = new double[numberOfLines]();
   double* x_chunk = new double[numberOfLines];
-  for (; acc >= eps * eps; ++iCount) {
+  for (; acc >= eps * eps;) {
     getAxb(Achunk, x, b, AxbChunk, numberOfLines, offset);
     getNextX(AxbChunk, x, x_chunk, numberOfLines, offset);
     MPI_Allgatherv(x_chunk, numberOfLines, MPI_DOUBLE, x, line_count,
@@ -155,7 +111,10 @@ double* simpleIterationMethod(double* Achunk, double* b, double* x,
     double AxbChunk_norm_square = norm(AxbChunk, numberOfLines);
     MPI_Reduce(&AxbChunk_norm_square, &acc, 1, MPI_DOUBLE, MPI_SUM, 0,
                MPI_COMM_WORLD);
-    if (processRank == 0) acc = acc / bNorm;
+    if (processRank == 0) {
+      acc = acc / bNorm;
+      ++iCount;
+    }
     MPI_Bcast(&acc, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
   }
   // if (processRank == 0) std::cout << iCount << std::endl;
@@ -172,8 +131,9 @@ inline void getNextX(const double* AxbChunk, const double* x, double* x_chunk,
 void getAxb(const double* Achunk, const double* x, const double* b,
             double* AxbChunk, int chunkSize, int chunkOffset) {
   for (int i = 0; i < chunkSize; ++i) {
-    AxbChunk[i] = -b[chunkOffset + i];
+    AxbChunk[i] = 0;
     for (int j = 0; j < N; ++j) AxbChunk[i] += Achunk[i * N + j] * x[j];
+    AxbChunk[i] -= b[chunkOffset + i];
   }
 }
 
